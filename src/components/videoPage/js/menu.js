f9ea1902c8ff786e5e5e6ff98f8b41928526388e
AsesinoDC <osorioemanuel0520@gmail.com>
2024-05-23 20:59:14 -0500

    Subir unas paginas mas

--- src/components/videoPage/js/menu.js
+++ src/components/videoPage/js/menu.js
@@ -1,16 +1,13 @@
-import {open, seAbrio, list, listIcon} from "../../../../menu.js"
+import {list ,listIcon} from "../../../../menu.js"
 
 const menuToggle = document.querySelector('.menuToggle');//selecciona el elemento con la clase menuToggle
 const navigation = document.querySelector('.navigation');//selecciona el elemento con la clase navigation
 
 const navBar = document.querySelector('#navBar');
-seAbrio(menuToggle, navigation);
 
-console.log(open.openBar);
-if (open.openBar == true){
-    console.log(open.openBar);
-    console.log("buenas");
-}
+menuToggle.onclick = function() {
+    navigation.classList.toggle('open');
+}//cuando se haga click en el menuToggle, se le agrega o quita la clase open al elemento con la clase navigation
 
 
 listIcon(list);
