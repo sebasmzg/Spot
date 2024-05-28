0a29489552242c51f1acb978b91ee5d281a0ce8b
Sebastian Muñoz <j.sebastianmzg@gmail.com>
2024-05-27 19:55:14 -0500

    se agrega  modal de busqueda

--- src/components/menu/js/menu.js
+++ src/components/menu/js/menu.js
@@ -1,25 +1,5 @@
-export let open = {
-    openBar: false
-};
-let contador = 0;
 const menuToggle = document.querySelector('.menuToggle');//selecciona el elemento con la clase menuToggle
 const navigation = document.querySelector('.navigation');//selecciona el elemento con la clase navigation
-export function seAbrio(menuToggle, navigation) {
-    menuToggle.onclick = function() {
-        navigation.classList.toggle('open');
-        contador = contador +1;
-        if(contador == 1){
-            open.openBar = true;
-            console.log(open.openBar);
-        }
-        else{
-            open.openBar = false;
-            contador = 0;
-            console.log(open.openBar);
-        }
-        
-    }//cuando se haga click en el menuToggle, se le agrega o quita la clase open al elemento con la clase navigation
-}
 
 //seAbrio(menuToggle, navigation);
 
--- src/components/search/js/search.js
+++ src/components/search/js/search.js
@@ -1,5 +1,24 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    var modal = document.getElementById('myModalSearch'); // obtiene el modal
+    var btn = document.getElementById('openSearchModal'); // obtiene el botón que abre el modal
+    var span = document.getElementsByClassName('close-Search')[0]; // obtiene el primer span (el de cerrar)
+
+    btn.onclick = function () {
+        modal.style.display = 'block'; // muestra el modal
+    }
+
+    span.onclick = function () { // cierra el modal si se hace clic en la 'x'
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function (event) { // cierra el modal si se hace clic fuera de él
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+
     // Eliminar elementos individuales al hacer clic en "✕".
     document.querySelectorAll('.eliminar').forEach(button => {
         button.addEventListener('click', (event) => {
