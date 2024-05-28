d0aa8cb78bc8f2b354505c736aa7049d35b935f5
Sebastian Muñoz <j.sebastianmzg@gmail.com>
2024-05-27 20:03:30 -0500

    actualiza

--- src/components/menu/js/menu.js
+++ src/components/menu/js/menu.js
@@ -1,18 +1,16 @@
 const menuToggle = document.querySelector('.menuToggle');//selecciona el elemento con la clase menuToggle
 const navigation = document.querySelector('.navigation');//selecciona el elemento con la clase navigation
 
-//seAbrio(menuToggle, navigation);
+menuToggle.onclick = function() {
+    navigation.classList.toggle('open');
+}//cuando se haga click en el menuToggle, se le agrega o quita la clase open al elemento con la clase navigation
 
-export const list = document.querySelectorAll('.list');
-export function listIcon(list) {
-    function activarLink(){
-        list.forEach((item)=>
-        item.classList.remove('active'));//remueve la clase active de todos los elementos con la clase list
-        this.classList.add('active');//agrega la clase active al elemento que se le hizo click
-    }
-    
+const list = document.querySelectorAll('.list');
+function activarLink(){
     list.forEach((item)=>
-    item.addEventListener('click', activarLink));//cuando se haga click en un elemento con la clase list, se ejecuta la función activarLink
+    item.classList.remove('active'));//remueve la clase active de todos los elementos con la clase list
+    this.classList.add('active');//agrega la clase active al elemento que se le hizo click
 }
 
-listIcon(list)
+list.forEach((item)=>
+item.addEventListener('click', activarLink));//cuando se haga click en un elemento con la clase list, se ejecuta la función activarLink
