2b79868f3fa6d53dd61757d3e7aed24348cf9356
Sebastian Muñoz <j.sebastianmzg@gmail.com>
2024-05-29 15:42:17 -0500

    funcionalidad botón logout

--- src/components/incomePage/js/logout.js
+++ src/components/incomePage/js/logout.js
@@ -3,5 +3,5 @@ const logout = document.querySelector('#logout'); // Botón de cerrar sesión
 logout.addEventListener('click', () => { // Evento click del botón de cerrar sesión
     confirm('¿Estás seguro de que deseas cerrar sesión?'); // Pregunta al usuario si está seguro de cerrar sesión
     localStorage.removeItem('login_success'); // Elimina el estado de login del localStorage
-    window.location.href = '../income.hmtl'; // Redirige al usuario a la página de ingresos
+    window.location.href = '/src/components/incomePage/income.html'; // Redirige al usuario a la página de ingresos
 });
