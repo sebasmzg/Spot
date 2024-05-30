8e2a25808001f52a580bffe21e69b475299d0081
santiagozuluaga23 <santiago.zuluagad2000@gmail.com>
2024-05-30 15:17:51 -0500

    pantalla precarga

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const userLogin = JSON.parse(localStorage.getItem('login_success')) || false; //
 if (!userLogin) {
     window.location.href = './src/components/incomePage/income.html'; 
 }
+
+//Para el botón de inicio
+const loginButton = document.getElementById("loginButton"); //Obetenemos una referencia al elemento del DOM
+loginButton.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
--- src/components/loadingScreen/js/loadingScreen.js
+++ src/components/loadingScreen/js/loadingScreen.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Archivo JavaScript con funcionalidad de ocultar la pantalla de carga y mostrar el contenido principal
 
 //Llama y selecciona el elemento de video con el ID 'loading-video' y agrega un listener para el evento 'ended'
@@ -8,24 +7,5 @@ document.getElementById('loading-video').addEventListener('ended', function() {
     //Llama y selecciona el elemento con el ID 'loading-screen' y oculta su display
     document.getElementById('loading-screen').style.display = 'none';
     //Su función es hacer que el contenedor de la pantalla de carga desaparezca
-
-    //Llama y selecciona el elemento con el ID 'main-content y muestra su display'
-    document.getElementById('main-content').style.display = 'block';
-    //Su función es hacer que el contenido principal de la página se muestre
+    
 });
-=======
-//Archivo JavaScript con funcionalidad de ocultar la pantalla de carga y mostrar el contenido principal
-
-//Llama y selecciona el elemento de video con el ID 'loading-video' y agrega un listener para el evento 'ended'
-document.getElementById('loading-video').addEventListener('ended', function() {
-    //La función se ejecutará cuando el video termine de reproducirse
-
-    //Llama y selecciona el elemento con el ID 'loading-screen' y oculta su display
-    document.getElementById('loading-screen').style.display = 'none';
-    //Su función es hacer que el contenedor de la pantalla de carga desaparezca
-
-    //Llama y selecciona el elemento con el ID 'main-content y muestra su display'
-    document.getElementById('main-content').style.display = 'block';
-    //Su función es hacer que el contenido principal de la página se muestre
-});
->>>>>>> 2a0d079fb78f20fe4cef8526bc87072905a000ea
